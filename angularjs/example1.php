bfec642c6a27d77b4f47fd6a80198f16be675504
Sailendranath Mansingh <sailendranathmansingh6@gmail.com>
2020-02-09 13:13:51 +0530

    One new line added

--- angularjs/example1.php
+++ angularjs/example1.php
@@ -2,4 +2,5 @@
 <div ng-app="">
 	<p>Name:<input type="text" name="stuname" ng-model="stuname"></p>
 	<p><h1>Hello <span>{{stuname}}</span></h1></p>
+	<p><h1>Hello <span>Sailendra</span></h1></p>
 </div>
